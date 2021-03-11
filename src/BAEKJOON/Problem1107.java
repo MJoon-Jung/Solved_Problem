00a3ada7d6a78415cc1fe79ab1e20323a15435b2
MJoon-Jung <gjgjajaj31@gmail.com>
2021-03-11 19:38:15 +0900

    BAEKJOON Problem

--- src/BAEKJOON/Problem1107.java
+++ src/BAEKJOON/Problem1107.java
@@ -3,18 +3,41 @@ package BAEKJOON;
 import java.util.Scanner;
 
 public class Problem1107 {
+	static boolean [] isTrue;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int t = sc.nextInt();
-		int [] ar = new int [t];
+		int m = sc.nextInt();
 		
-		for(int i = 0; i < t; i++) {
-			ar[i] = sc.nextInt();
+		isTrue = new boolean [10];
+		
+		for(int i = 0; i < m; i++) {
+			int x = sc.nextInt();
+			isTrue[x] = true;
 		}
 		
-		int c = 100;
-		int cnt = 0;
+		int answer = Math.abs(n - 100);
 		
+		for(int i = 0; i <=  1000000; i++) {
+			int length = isPossible(i);
+			if(length > 0) answer = Math.min(answer, Math.abs(n - i) + length);
+		}
+		
+		System.out.println(answer);
+		
+	}
+	public static int isPossible(int n) {
+		if(n == 0){
+			if(isTrue[n]) return 0;
+			else return 1;
+		}
+		int len = 0;
+		
+		while(n > 0){
+			if(isTrue[n % 10]) return 0;
+			len++;
+			n /= 10;
+		}
+		return len;
 	}
 }
